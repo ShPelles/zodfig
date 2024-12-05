8912c1202ebf47c79d6734e465702f1164cbb191
ShPelles <43875468+ShPelles@users.noreply.github.com>
2024-12-05 12:39:17 +0200

    Allow to use a few readers with fallback mechanism (#3)

--- test/zodfig.test.ts
+++ test/zodfig.test.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { zodfig } from "../src/zodfig";
 import { expect, test, describe } from "vitest";
+import ObjReader from "../src/obj-reader";
 
 describe("zodfig()", () => {
   test("should return an object with a read function", () => {
@@ -23,11 +24,43 @@ describe("zodfig()", () => {
       str2: z.string(),
     });
     // Act
-    const result = zodfig(schema, customReader).read();
+    const result = zodfig(schema, [customReader]).read();
     // Assert
     expect(result).toEqual({
       str1: "str1Value",
       str2: "str2Value",
+    });
+  });
+
+  test("should use fallback mechanism with multiple readers", () => {
+    // Arrange
+    const reader1 = new ObjReader({ str1: "value1" });
+    const reader2 = new ObjReader({ str2: "value2" });
+    const schema = z.object({
+      str1: z.string(),
+      str2: z.string(),
+    });
+    // Act
+    const result = zodfig(schema, [reader1, reader2]).read();
+    // Assert
+    expect(result).toEqual({
+      str1: "value1",
+      str2: "value2",
+    });
+  });
+
+  test("should respect the order of readers in the fallback mechanism", () => {
+    // Arrange
+    const reader1 = new ObjReader({ str: "value1" });
+    const reader2 = new ObjReader({ str: "value2" });
+    const schema = z.object({
+      str: z.string(),
+    });
+    // Act
+    const result = zodfig(schema, [reader1, reader2]).read();
+    // Assert
+    expect(result).toEqual({
+      str: "value1",
     });
   });
 });
